b09717b08c943b740b2e9b60361dbf4419371c41
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-05 16:02:34 +0000

    Test commit

---
+++ plugins/tor/tor.rb
@@ -0,0 +1,24 @@
+  module AresMUSH
+    module Tor
+
+   def self.plugin_dir
+     File.dirname(__FILE__)
+   end
+
+   def self.shortcuts
+     Global.read_config("tor", "shortcuts")
+   end
+
+   def self.get_cmd_handler(client, cmd, enactor)
+     nil
+   end
+
+   def self.get_event_handler(event_name)
+     nil
+   end
+
+   def self.get_web_request_handler(request)
+     nil
+   end
+
+ end
