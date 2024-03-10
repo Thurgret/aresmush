bff7e52eca691ce8333945b10bcbff9d6f18829d
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-10 22:44:26 +0000

    test

--- plugins/tor/commands/roll_cmd.rb
+++ plugins/tor/commands/roll_cmd.rb
@@ -17,7 +17,7 @@ module AresMUSH
       
       def check_modifier
         return nil if self.modifier.blank?
-        return t('tor.invalid_difficulty') if !modifier.is_a Integer
+        return t('tor.invalid_difficulty') if !modifier.is_a? Integer
         return nil
       end
       
