040b507e332ab7ec40cdcfd1069532da11ddca13
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-11 03:10:01 +0000

    test

--- plugins/tor/commands/roll_cmd.rb
+++ plugins/tor/commands/roll_cmd.rb
@@ -35,7 +35,7 @@ module AresMUSH
           elsif (results.eye_of_mordor)
             message = t('tor.roll_eye_of_mordor_success', :dice => results.dice.join(" "), :roll => self.roll_str, :char => enactor_name )
           else
-            message = t('tor.roll_successful', :dice => results.dice.join(" "), :roll => self.roll_str, :char => enactor_name )
+            message = t('tor.roll_successful', :dice => results.dice.join(" "), :feat_die => results.feat_die.to_s, :roll => self.roll_str, :char => enactor_name )
           end
         end
           
@@ -44,7 +44,7 @@ module AresMUSH
             if (results.eye_of_mordor)
               message = t('tor.eye_of_mordor_failure', :dice => results.dice.join(" "), :roll => self.roll_str, :char => enactor_name )
             else
-              message = t('tor.roll_failure', :dice => results.dice.join(" "), :feat_die => results.feat_die(' '),  :roll => self.roll_str, :char => enactor_name )
+              message = t('tor.roll_failure', :dice => results.dice.join(" "), :feat_die => results.feat_die.to_s,  :roll => self.roll_str, :char => enactor_name )
             end
           
           end
