35091116d319d5fdaf512d82901741ec3d67d1ab
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-12 04:42:07 +0000

    test

--- plugins/tor/culture_setup.rb
+++ plugins/tor/culture_setup.rb
@@ -5,6 +5,7 @@ module AresMUSH
         
         def self.culture_skills(model, culture_name)
             culture = find_culture(model, culture_name)
+            name = culture.name_downcase
 
             #This is a profoundly bad way to do this and in the very unlikely event anyone ever looks at this, don't do this.
          
@@ -28,7 +29,7 @@ module AresMUSH
             travel =Tor.find_skill(model, "travel")
 
 
-            if (culture_name == "bardings")
+            if (name == "bardings")
                 athletics.update(rating: 1)
                 awe.update(rating: 1)
             end
