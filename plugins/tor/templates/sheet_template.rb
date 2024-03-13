e110888e38d197ce8ba72ee421a2e429fb0502ca
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-13 02:48:01 +0000

    test

--- plugins/tor/templates/sheet_template.rb
+++ plugins/tor/templates/sheet_template.rb
@@ -19,6 +19,17 @@ module AresMUSH
                 "#{linebreak}%xh#{title}%xn #{rating}"
           end
         end
+
+        def format_tn_two_per_line(list)
+          list.to_a.sort_by { |a| a.name }
+            .each_with_index
+              .map do |a, i| 
+                linebreak = i % 2 == 0 ? "\n" : ""
+                title = left("#{ a.name }:", 15)
+                targetnumbers = left(a.target_number, 20)
+                "#{linebreak}%xh#{title}%xn #{target_number}"
+          end
+        end
         
         def skills
           format_two_per_line @char.tor_skills
@@ -31,7 +42,7 @@ module AresMUSH
         end
 
         def targetnumbers
-          format_two_per_line @char.tor_tn
+          format_tn_two_per_line @char.tor_tn
         end
         
       end
