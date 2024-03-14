33f16e2ba97a3ce41d5ecdc4b5e908abaaf1cc03
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-14 21:18:09 +0000

    test

--- plugins/tor/templates/sheet_template.rb
+++ plugins/tor/templates/sheet_template.rb
@@ -46,7 +46,7 @@ module AresMUSH
         end
 
         def attributes
-          left("Strength:" + @char.tor_attributes.find(:name => "Strength") )
+          left("Strength:" + attribute_rating(@char, "strength").to_s )
           "Parry: " + @char.tor_parry.to_s
         end
         
