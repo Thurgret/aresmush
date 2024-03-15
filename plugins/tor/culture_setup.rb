d6b10ff5f9aa94bf7724a9781d6e51fec120f1c1
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-15 23:03:19 +0000

    test

--- plugins/tor/culture_setup.rb
+++ plugins/tor/culture_setup.rb
@@ -17,7 +17,7 @@ module AresMUSH
             
 
             Tor.select_attributes(model, 5.to_s)
-            Global.logger.debug Tor.attribute_rating(model, "strength")
+            Global.logger.debug Tor.attribute_rating(model, "strength").to_s
             Tor.set_initial_derived_stats(model)
             Tor.set_valour(model, 1)
             Tor.set_wisdom(model, 1)
