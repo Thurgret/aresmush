b7fdd2dda60ff61c2dac679df84b7b3ca0563811
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-17 18:38:04 +0000

    test

--- plugins/tor/templates/sheet_template.rb
+++ plugins/tor/templates/sheet_template.rb
@@ -77,8 +77,8 @@ module AresMUSH
         end
 
         def adventure_and_skill_points
-          firstline = left(("Current Adventure Points: " + @char.tor_adventure_points.to_s), 40) + right(("Total Adventure Points Earned: " + @char.tor_lifetime_adventure_points.to_s), 40)
-          secondline = left(("Current Skill Points: " + @char.tor_skill_points.to_s), 40) + right(("Total Skill Points Earned: " + @char.tor_lifetime_skill_points.to_s), 40)
+          firstline = left(("Current Adventure Points: " + @char.tor_adventure_points.to_s), 35) + right(("Total Adventure Points: " + @char.tor_lifetime_adventure_points.to_s), 35)
+          secondline = left(("Current Skill Points: " + @char.tor_skill_points.to_s), 35) + right(("Total Skill Points: " + @char.tor_lifetime_skill_points.to_s), 35)
           "" + firstline + "\n" + secondline + "\n"
         end
 
