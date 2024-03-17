dd104fa3542ac40f352c41399adfdc18477c6ac4
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-17 18:57:16 +0000

    test

--- plugins/tor/commands/starting_virtue_set_cmd.rb
+++ plugins/tor/commands/starting_virtue_set_cmd.rb
@@ -32,8 +32,8 @@ module AresMUSH
           ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
             virtue_config = Tor.find_virtue_config(self.virtue_name)
             virtue_culture = virtue_config["culture"]
-            if (virtue_culture.downcase != "everyone")
-                   return t('tor.invalid_virtue')
+            if (virtue_culture.downcase != "everyone" && !Tor.can_manage_abilities?(enactor))
+                   return t('tor.invalid_virtue_not_for_everyone')
             end
                
           return nil if enactor_name == self.target_name
