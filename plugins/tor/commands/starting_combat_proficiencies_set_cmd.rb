39b05f4173ba171f0f3b853fb08a3566452712b5
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-17 23:50:06 +0000

    test

--- plugins/tor/commands/starting_combat_proficiencies_set_cmd.rb
+++ plugins/tor/commands/starting_combat_proficiencies_set_cmd.rb
@@ -7,6 +7,7 @@ module AresMUSH
         
         def parse_args
           # Admin version
+        
           if (cmd.args =~ /=/)
             args = cmd.parse_args(ArgParser.arg1_equals_arg2_slash_arg3)
             self.target_name = titlecase_arg(args.arg1)
@@ -27,32 +28,41 @@ module AresMUSH
         def check_valid_proficiency
             if (["Axes", "Bows", "Spears", "Swords"].exclude?(self.firstproficiency) || ["Axes", "Bows", "Spears", "Swords"].exclude?(self.secondproficiency))
                 return t('tor.invalid_proficiency_name')
+            end
             return nil
         end
         
         def check_can_set
-          ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
+          
+           
+            
+           
+            ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
             culture_name = model.group("Culture").downcase
             proficiency_config = Tor.find_combat_proficiencies_config(culture_name)
+            
             if (proficiency_config.exclude?(self.firstproficiency))
                    return t('tor.proficiency_not_available')
             end
             if (firstproficiency == secondproficiency)
                 return t('tor.same_proficiency_selected')
+            
             end
                
           return nil if enactor_name == self.target_name
           return nil if Tor.can_manage_abilities?(enactor)
           return t('dispatcher.not_allowed')
-          end
-        end     
+          
+        
+        end
+       
         
         def check_chargen_locked
           return nil if Tor.can_manage_abilities?(enactor)
           Chargen.check_chargen_locked(enactor)
+       
         end
         
-       
         def handle
           ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
 
@@ -61,11 +71,14 @@ module AresMUSH
             Tor.set_combat_proficiency(model, secondproficiency, 2)
             
             client.emit_success t('tor.virtue_set')
-          end
-           
+          
         end
          
-          
+         
     end
+       
+end
       
 end
+    
+  
