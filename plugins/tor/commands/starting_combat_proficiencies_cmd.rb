27bff86c836b77c030608074026dd5e9d326b13f
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-17 23:01:36 +0000

    test

--- plugins/tor/commands/starting_combat_proficiencies_cmd.rb
+++ plugins/tor/commands/starting_combat_proficiencies_cmd.rb
@@ -12,7 +12,8 @@ module AresMUSH
         
                 ClassTargetFinder.with_a_character(enactor_name, client, enactor) do |model|
                 # Get the hash of attributes out of the configuration
-                profiencies = Tor.find_combat_proficiencies_config(model)
+                culture_name = model.group("Culture").to_s
+                profiencies = Tor.find_combat_proficiencies_config(culture_name)
           
                 # Sort the hash and then convert it to a list of the form "Name Description"
                 list = "You can select " + profiencies["options1"] + " or " + profiencies["options"] + " to have a starting rating of 2, and select any other combat proficiency, whether axes, bows, spears or swords, to have a starting rating of 1.\nAn example of the command syntax is startingcombatproficiencies/set spears/bows."
