2d5bb18ee48079b33eaaa400323b22bf0b35f7b3
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-17 23:03:04 +0000

    test

--- plugins/tor/commands/starting_combat_proficiencies_cmd.rb
+++ plugins/tor/commands/starting_combat_proficiencies_cmd.rb
@@ -12,11 +12,11 @@ module AresMUSH
         
                 ClassTargetFinder.with_a_character(enactor_name, client, enactor) do |model|
                 # Get the hash of attributes out of the configuration
-                culture_name = model.group("Culture").to_s
+                culture_name = model.group("Culture").downcase
                 profiencies = Tor.find_combat_proficiencies_config(culture_name)
           
                 # Sort the hash and then convert it to a list of the form "Name Description"
-                list = "You can select " + profiencies["options1"] + " or " + profiencies["options"] + " to have a starting rating of 2, and select any other combat proficiency, whether axes, bows, spears or swords, to have a starting rating of 1.\nAn example of the command syntax is startingcombatproficiencies/set spears/bows."
+                list = "You can select " + profiencies["options1"].to_s + " or " + profiencies["options2"].to_s + " to have a starting rating of 2, and select any other combat proficiency, whether axes, bows, spears or swords, to have a starting rating of 1.\nAn example of the command syntax is startingcombatproficiencies/set spears/bows."
          
                   # Use the standard bordered list template to show the list with a title above it.
                   client.emit list
