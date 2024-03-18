c0f3d0032f7542881742c04cce6984e4e9863c7d
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-18 18:12:10 +0000

    test

--- plugins/tor/gear.rb
+++ plugins/tor/gear.rb
@@ -14,7 +14,7 @@ module AresMUSH
             type = config["type"]
             Global.logger.debug name
             Global.logger.debug type
-            TorArmour.create(:name => config["name"], :type => config["type"], :gearload => config["gearload"], :protection => config["protection"])           
+            TorArmour.create(:name => config["name"], :type => config["type"], :gearload => config["load"], :protection => config["protection"])           
 
         end
 
