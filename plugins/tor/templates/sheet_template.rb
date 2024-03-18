693afb60574bcb77c25acf02c41e89a09a351cfa
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-18 17:47:11 +0000

    test

--- plugins/tor/templates/sheet_template.rb
+++ plugins/tor/templates/sheet_template.rb
@@ -94,9 +94,9 @@ module AresMUSH
               .map do |a, i| 
                 linebreak = i % 1 == 0 ? "\n" : ""
                 title = left("#{ a.name }:", 15)
-                rating = left(a.protection, 20)
-                gearload = center(a.gearload)
-                rewards = left(a.rewards.to_s)
+                rating = left("#{a.protection}:", 20)
+                gearload = center("#{a.gearload}:", 20)
+                rewards = left("#{a.rewards.to_s}:", 80)
                 "#{linebreak}%xh#{title}%xn #{rating} #{gearload}\n#{rewards}"
           end
         end
