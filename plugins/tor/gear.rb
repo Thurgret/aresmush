a040d894ee8daa1c6355693a89c0d383cc986b24
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-19 01:22:22 +0000

    test

--- plugins/tor/gear.rb
+++ plugins/tor/gear.rb
@@ -93,7 +93,7 @@ module AresMUSH
                 end
             else
                 if(weapon.hands == "either")
-                    two_handed_injury = weapon["injury"] + 2
+                    two_handed_injury = weapon.injury + 2
                     weapon.update(:injury => two_handed_injury)
                     model.update(:second_hand_in_use => true)
                 elsif(weapon.hands == "both")
