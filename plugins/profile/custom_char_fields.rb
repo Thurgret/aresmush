d7210ba271accc8a03410f72bd69882a8a02a851
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-21 17:21:25 +0000

    test

--- plugins/profile/custom_char_fields.rb
+++ plugins/profile/custom_char_fields.rb
@@ -114,10 +114,10 @@ module AresMUSH
         attribute_options = Tor.find_attribute_options_config(culture)
         finalattributeoptions = ""
 
-        attribute_options.to_a do |a|
-          finalattributeoptions = finalattributeoptions + a.rating.to_s + "\n"
+        #attribute_options.to_a do |a|
+         # finalattributeoptions = finalattributeoptions + a.rating.to_s + "\n"
           
-        end
+        #end
 
 
 
@@ -138,7 +138,7 @@ module AresMUSH
     healing: Website.format_markdown_for_html(healing_string), courtesy: Website.format_markdown_for_html(courtesy_string), battle: Website.format_markdown_for_html(battle_string),
     persuade: Website.format_markdown_for_html(persuade_string), stealth: Website.format_markdown_for_html(stealth_string), scan: Website.format_markdown_for_html(scan_string),
     explore: Website.format_markdown_for_html(explore_string), riddle: Website.format_markdown_for_html(riddle_string), lore: Website.format_markdown_for_html(lore_string),
-    virtue: Website.format_markdown_for_html(virtue_string), attribute_options: ["1", "2", "3", "4"] }
+    virtue: Website.format_markdown_for_html(virtue_string), attribute_options: attribute_options }
       end
       
       # Saves fields from profile editing.
