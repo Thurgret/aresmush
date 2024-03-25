d6b6e17d87634aa36dfa8ca121f0033c480f6f22
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-25 01:21:40 +0000

    test

--- plugins/tor/helpers.rb
+++ plugins/tor/helpers.rb
@@ -188,7 +188,7 @@ module AresMUSH
             elsif (culture == "bree-folk")
                 return "Bree-blood: Each of the Bree-folk in the Company increases the Fellowship Rating by 1 point."
             elsif (culture == "dwarves of durin's folk")
-                return "Redoubtable: You halve the Load rating of any armour you're wearing (rounding fractions up), including helms (but not shields).\n
+                return "Redoubtable: You halve the Load rating of any armour you're wearing (rounding fractions up), including helms (but not shields).
                 Naugrim: Dwarven adventurers cannot use the following pieces of war gear: great bow, great spear, and great shield."
             elsif (culture == "elves of lindon")
                 return "Elven-skill: If you are not Miserable, you can spend 1 point of Hope to achieve a Magical success on a roll when using a Skill in which you possess at least one rank."    
@@ -196,10 +196,10 @@ module AresMUSH
                 return "Elven-wise: Add 1 point to one Attribute of your choice. Additionally, if you are not Miserable, you can spend 1 point of Hope to achieve a Magical success on a skill roll.
                 Beset by Woe: You can remove accumulated Shadow points exclusively during a Yule Fellowship Phase."
             elsif (culture == "hobbits of the shire")
-                return "Hobbit-sense: Your WISDOM rolls are Favoured, and you gain (1d) on all Shadow Tests made to resist the effects of Greed.\n
+                return "Hobbit-sense: Your WISDOM rolls are Favoured, and you gain (1d) on all Shadow Tests made to resist the effects of Greed.
                 Halflings: Axe, bow, club, cudgel, dagger, short sword, short spear, spear. Additionally, Hobbits cannot use a great shield."    
             elsif (culture == "rangers of the north")
-                return "Kings of Men: Add 1 point to one Attribute of your choice.\n
+                return "Kings of Men: Add 1 point to one Attribute of your choice.
                 Allegiance of the Dúnedain: During the Fellow ship phase (not Yule) you recover a maximum number of Hope points equal to half your HEART score (rounding fractions up)."
             end
         end
