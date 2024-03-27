a0318ccf0b1e771ed5f80b3ab73db4aebbac000c
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-03-27 23:38:31 +0000

    test

--- plugins/profile/custom_char_fields.rb
+++ plugins/profile/custom_char_fields.rb
@@ -279,13 +279,10 @@ module AresMUSH
 
 
 
-        if (first_weapon_proficiency != "-" && first_weapon_proficiency != "" && first_weapon_proficiency)
+        if (first_weapon_proficiency != "-" && first_weapon_proficiency != "" && first_weapon_proficiency
+          && second_weapon_proficiency != "-" && second_weapon_proficiency != "" && second_weapon_proficiency)
           Tor.zero_combat_proficiencies(charmodel)
           Tor.set_combat_proficiency(charmodel, first_weapon_proficiency, 2)
-        end
-        if (second_weapon_proficiency != "-" && second_weapon_proficiency != "" && second_weapon_proficiency)
-          
-        Tor.zero_combat_proficiencies(charmodel)
           Tor.set_combat_proficiency(charmodel, second_weapon_proficiency, 1)
         end
 
