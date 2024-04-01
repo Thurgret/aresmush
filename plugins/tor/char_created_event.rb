8e81cdad37b40af94ba13a345ed5936f69341b13
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-04-01 03:20:13 +0100

    test

--- plugins/tor/char_created_event.rb
+++ plugins/tor/char_created_event.rb
@@ -1,6 +1,7 @@
 module AresMUSH
     module Tor
       def self.get_event_handler(event_name) 
+        Global.logger.debug event_name
         case event_name
         when "CharCreatedEvent"
           return CharSetInitialCulture
--- plugins/tor/char_set_initial_culture.rb
+++ plugins/tor/char_set_initial_culture.rb
@@ -3,6 +3,7 @@ module AresMUSH
       class CharSetInitialCulture
         def on_event(event)
            char = Character[event.char_id]
+           Global.logger.debug char.name
            Demographics.set_group(model, "Culture", "Hobbits of the Shire")
            Demographics.set_group(model, "Calling", "Treasure Hunter")
            Tor.initial_setup(char)
