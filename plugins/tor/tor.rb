730a7fcf3f3229578ccbdd4db8978a9b8bb5ae0a
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-04-01 03:30:35 +0100

    test

--- plugins/tor/tor.rb
+++ plugins/tor/tor.rb
@@ -122,6 +122,11 @@ module AresMUSH
 
  
     def self.get_event_handler(event_name)
+      Global.logger.debug event_name
+        case event_name
+        when "CharCreatedEvent"
+          return CharCreatedEventHandler
+        end
       nil
    end
 
