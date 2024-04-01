182bd7f6d3905529bb3de0cceda9b285ebdf4086
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-04-01 03:32:24 +0100

    test

--- plugins/tor/char_created_event_handler.rb
+++ plugins/tor/char_created_event_handler.rb
@@ -4,8 +4,8 @@ module AresMUSH
         def on_event(event)
            char = Character[event.char_id]
            Global.logger.debug char.name
-           Demographics.set_group(model, "Culture", "Hobbits of the Shire")
-           Demographics.set_group(model, "Calling", "Treasure Hunter")
+           Demographics.set_group(char, "Culture", "Hobbits of the Shire")
+           Demographics.set_group(char, "Calling", "Treasure Hunter")
            Tor.initial_setup(char)
         end
       end
