66364d13305ba3bb618804074a74ca3494c81d9e
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-04-01 21:38:49 +0100

    test

--- plugins/tor/char_created_event_handler.rb
+++ plugins/tor/char_created_event_handler.rb
@@ -3,7 +3,6 @@ module AresMUSH
       class CharCreatedEventHandler
         def on_event(event)
            char = Character[event.char_id]
-           Global.logger.debug char.name
            Demographics.set_group(char, "Culture", "Hobbits of the Shire")
            Demographics.set_group(char, "Calling", "Treasure Hunter")
            Tor.initial_setup(char)
--- plugins/tor/tor.rb
+++ plugins/tor/tor.rb
@@ -122,7 +122,6 @@ module AresMUSH
 
  
     def self.get_event_handler(event_name)
-      Global.logger.debug event_name
         case event_name
         when "CharCreatedEvent"
           return CharCreatedEventHandler
