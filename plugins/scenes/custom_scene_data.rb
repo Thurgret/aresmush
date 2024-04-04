4ad63b83faebc15f59a05a740da4c74c4ccb9c47
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-04-04 23:30:32 +0100

    test

--- plugins/scenes/custom_scene_data.rb
+++ plugins/scenes/custom_scene_data.rb
@@ -12,7 +12,7 @@ module AresMUSH
       
       
 #       Otherwise return a hash of data.  For example, if you want to use your custom plugin's abilities, you might do:
-       {
+       return {
          torcombatabilities: torcombatabilities
        }
     end
