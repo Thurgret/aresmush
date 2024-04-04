544b72ae806b87e5f09086c8ffd33f425242deff
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-04-05 00:20:50 +0100

    test

--- plugins/scenes/custom_scene_data.rb
+++ plugins/scenes/custom_scene_data.rb
@@ -5,19 +5,19 @@ module AresMUSH
       # Return nil if you don't need any custom data.
       
       
-        torcombatabilitiesarray = ["Axes", "Bows", "Spears", "Swords", "Protection"]
+        toradversaries = ["-"]
 
 
         
         Adversary.all.each do |a|
-        torcombatabilitiesarray <<  a.name
+        toradversaries <<  a.name
         end
       
       
       
 #       Otherwise return a hash of data.  For example, if you want to use your custom plugin's abilities, you might do:
        return {
-         torcombatabilities: torcombatabilitiesarray
+         toradversaries: torcombatabilitiesarray
        }
     end
   end
