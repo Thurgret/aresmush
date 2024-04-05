2bb5bcd895955a09eb19ec869fb4e82c5ca7d9b5
Thurgret <158625520+Thurgret@users.noreply.github.com>
2024-04-05 01:16:34 +0100

    test

--- plugins/tor/rolls.rb
+++ plugins/tor/rolls.rb
@@ -433,7 +433,6 @@ module AresMUSH
               if (a.wielded == "in hand")
                 Global.logger.debug "doing stuff here"
                   skill_name = a.proficiency
-                  Global.logger.debug skill_name
                   weapon_name = a.name
                   damage = a.damage
                   injury = a.injury
@@ -444,6 +443,8 @@ module AresMUSH
          
             end
 
+            Global.logger.debug "skill name"
+            Global.logger.debug skill_name
 
             
 
